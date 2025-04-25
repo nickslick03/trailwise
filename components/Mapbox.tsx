fde9ccf7f6a39da9f92cb828f1c1f8a29b4bf409
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-04-25 14:15:05 -0400

    Added error handling to mapbox

--- components/Mapbox.tsx
+++ components/Mapbox.tsx
@@ -1,23 +1,36 @@
 // components/Map.js
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN; // Replace with your Mapbox access token
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 
 const Mapbox = () => {
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: 'map', // ID of the element to render the map
-      style: 'mapbox://styles/mapbox/streets-v11', // Map style
-      center: [-77.4, 41.5], // Starting position [lng, lat]
-      zoom: 9 // Starting zoom level
-    });
+  const [error, setError] = useState(false);
 
-    // Cleanup on unmount
-    return () => map.remove();
+  useEffect(() => {
+    try {
+      const map = new mapboxgl.Map({
+        container: 'map', // ID of the element to render the map
+        style: 'mapbox://styles/mapbox/streets-v11', // Map style
+        center: [-77.4, 41.5], // Starting position [lng, lat]
+        zoom: 9 // Starting zoom level
+      });
+
+      // Cleanup on unmount
+      return () => map.remove();
+    } catch (error) {
+      console.error('Error initializing Mapbox:', error);
+      setError(true);
+    }
   }, []);
 
-  return <div id='map' className='w-full h-full' />;
+  return error ? (
+    <div className='w-full h-full flex justify-center items-center'>
+      Error Initializing Mapbox
+    </div>
+  ) : (
+    <div id='map' className='w-full h-full' />
+  );
 };
 
 export default Mapbox;
