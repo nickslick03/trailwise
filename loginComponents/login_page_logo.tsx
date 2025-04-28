ec092366bfc9c3f22ee846c1aed9b28d87073fd7
nh-1984 <rocknrun412@gmail.com>
2025-04-28 12:30:24 -0400

    updated the logo for the login page

--- loginComponents/login_page_logo.tsx
+++ loginComponents/login_page_logo.tsx
@@ -2,10 +2,15 @@ import React from "react";
 
 const LoginPageLogo = () => {
   return (
-    <div className="flex justify-center items-center py-4">
-      <img src="/logo.png" alt="Trailwise Logo" className="h-24 w-auto" />
+    <div className="relative w-full h-full flex justify-center items-center py-4">
+      <img
+        src="/logo.png"
+        alt="Trailwise Logo"
+        className="object-contain w-[600px] h-[600px] sm:w-[800px] sm:h-[800px] lg:w-[1000px] lg:h-[1000px]"
+      />
     </div>
   );
 };
 
 export default LoginPageLogo;
+
