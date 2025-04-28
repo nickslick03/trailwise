168a29e17e170503b6938fe6f7cc6ead6b5dc346
nh-1984 <rocknrun412@gmail.com>
2025-04-28 10:05:21 -0400

    changed the background image so its not pixelated

--- loginComponents/login_page_background_image.tsx
+++ loginComponents/login_page_background_image.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const LoginPageBackgroundImage = () => {
     return (
       <div className="flex justify-center items-center py-4">
-        <img src="/mountain.png" alt="Trailwise Background" className="fixed top-0 left-0 w-full h-full bg-cover bg-center z-50" />
+        <img src="/mountain2.png" alt="Trailwise Background" className="fixed top-0 left-0 w-full h-full bg-cover bg-center z-50" />
       </div>
     );
   };
