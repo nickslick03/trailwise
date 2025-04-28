a7161ad5af37676c2f0761c8a5fccf1a792c666b
eespenshade1241 <ee1241@messiah.edu>
2025-04-28 15:21:17 -0400

    Created both Search pages, by the Map and by Regulations. Also modified some components

---
+++ app/searchReg/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import Head from "next/head";
+import Search from "@/components/Search";
+import Mapbox from "@/components/Mapbox";
+import FilterComponent from "@/components/FilterComponent";
+import BlackBoxPark from "@/components/BlackBoxPark";
+//import NavigationBar from "@/components/NavigationBar";
+
+export default function SearchMap() {
+    return (
+        <div className="fixed inset-0 flex items-center justify-center p-4 bg-gray-100">
+            <Head>
+                <title>Search Parks</title>
+            </Head>
+
+            <main className="max-w-md w-full mx-auto bg-white rounded-3xl shadow-xl overflow-hidden h-[90vh]">
+                <div className="p-4">
+
+                    <div className="flex items-center justify-between p-4 bg-white rounded-lg shadow-sm border border-gray-200 w-full max-w-md h-10">
+                        {/* Text on the left */}
+                        <span className="text-gray-700 font-medium">
+                            Miles from Current Location
+                        </span>
+                        
+                        {/* Dropdown on the right */}
+                        <select 
+                            className="bg-gray-50 border border-gray-300 text-gray-700 py-1 px-3 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 h-8"
+                        >
+                            <option value="10">10</option>
+                            <option value="25">25</option>
+                            <option value="50">50</option>
+                            <option value="100">100</option>
+                        </select>
+
+                        
+                    </div>
+
+                    <br/>
+                    <h1 className="text-gray-800 text-lg">Search By Regulation</h1>
+
+                    {/* Scrollable BlackBox content area */}
+                    <div className="flex-1 overflow-y-auto h-93">
+                        <BlackBoxPark />
+                        <BlackBoxPark />
+                        <BlackBoxPark />
+                        {/* Add more BlackBoxPark components as needed */}
+                    </div>
+                    
+                    {/* Filter Component */}
+                    <div className="sticky bottom-0 bg-white border-t">
+                        <FilterComponent />
+                    </div>
+                    {/* <NavigationBar /> */}
+                </div>
+            </main>
+        </div>
+    )
+}
--- components/FilterComponent.tsx
+++ components/FilterComponent.tsx
@@ -7,14 +7,14 @@ import { BsFillFilterCircleFill } from "react-icons/bs";
 function FilterComponent() {
     return (
         // Outer box
-        <div className="flex flex-col p-4 bg-white rounded-2xl shadow-md w-full max-w-fit m-auto">
+        <div className="fixed bottom-24 left-1/2 transform -translate-x-1/2 flex flex-col p-4 bg-white rounded-2xl shadow-md w-full max-w-sm max-w-fit">
             {/* Title Section */}
-            <div className='flex flex-row gap-2 items-center'>
+            <div className='flex flex-row gap-2 items-center text-gray-500'>
                 <BsFillFilterCircleFill></BsFillFilterCircleFill>
-                <div className="text-xl">Filters</div>
+                <div className="text-xl text-gray-500">Filters</div>
             </div>
             {/* Filters */}
-            <div className='flex flex-row gap-4 ml-6'>
+            <div className='flex flex-row gap-4 ml-6 text-gray-500'>
                 <div className='flex flex-row gap-1'>
                     <input type='checkbox'></input>
                     <div>Horse Trail</div>
