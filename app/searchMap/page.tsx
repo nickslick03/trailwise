20880de3721db7697b03a8571f258d70fafee483
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-04-28 22:45:58 -0400

    Added parks to map

--- app/searchMap/page.tsx
+++ app/searchMap/page.tsx
@@ -1,26 +1,39 @@
-"use client";
+'use client';
 
-import Head from "next/head";
-import Search from "@/components/Search";
-import Mapbox from "@/components/Mapbox";
-import FilterComponent from "@/components/FilterComponent";
+import Head from 'next/head';
+import Search from '@/components/Search';
+import Mapbox from '@/components/Mapbox';
+import FilterComponent from '@/components/FilterComponent';
+import { useEffect, useState } from 'react';
+import { Park } from '@/types/park';
 //import NavigationBar from "@/components/NavigationBar";
 
 export default function SearchMap() {
-    return (
-        <div className="fixed inset-0 flex items-center justify-center p-4 bg-gray-100">
-            <Head>
-                <title>Search Parks</title>
-            </Head>
+  const [parks, setParks] = useState<Park[]>([]);
 
-            <main className="max-w-md w-full mx-auto bg-white rounded-3xl shadow-xl overflow-hidden h-[90vh]">
-                <Mapbox parks={[]} />
-                <Search />
-                <FilterComponent />
-                {/* <NavigationBar /> */}
-                
-            </main>
-        </div>
-    )
+  useEffect(() => {
+    fetch('/api/parkLocations').then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      response.json().then((data) => {
+        setParks(data);
+      });
+    });
+  }, []);
 
+  return (
+    <div className='fixed inset-0 flex items-center justify-center p-4 bg-gray-100'>
+      <Head>
+        <title>Search Parks</title>
+      </Head>
+
+      <main className='max-w-md w-full mx-auto bg-white rounded-3xl shadow-xl overflow-hidden h-[90vh]'>
+        <Mapbox parks={parks} />
+        <Search />
+        <FilterComponent />
+        {/* <NavigationBar /> */}
+      </main>
+    </div>
+  );
 }
