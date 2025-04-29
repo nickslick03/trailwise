cb040e662ddf374712aca96195b62ea434f081f5
alexkagoro <ak1478@messiah.edu>
2025-04-28 20:13:16 -0400

    Update NavigationBar.tsx

---
+++ components/NavigationBar.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React from 'react';
+
+interface NavbarProps {
+  onNavigate: (page: string) => void;
+}
+
+const NavigationBar: React.FC<NavbarProps> = ({ onNavigate }) => {
+  return (
+    <nav className="w-full max-w-md p-4 fixed bottom-30 bg-gray-800 h-16 flex justify-around items-center border-t border-gray-700">
+      <button
+        className="flex flex-col items-center text-white text-xs focus:outline-none"
+        onClick={() => onNavigate('explore')}
+      >
+        <span className="text-2xl">🔍</span>
+        <span>Explore</span>
+      </button>
+
+      <button
+        className="flex flex-col items-center text-white text-xs focus:outline-none"
+        onClick={() => onNavigate('saved')}
+      >
+        <span className="text-2xl">💾</span>
+        <span>Saved</span>
+      </button>
+    </nav>
+  );
+};
+
+export default NavigationBar;
