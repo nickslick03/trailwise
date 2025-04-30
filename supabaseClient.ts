e90a38258df2256e80f17b55111e3bb19320bd91
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-04-30 08:21:16 -0400

    Removed misnomered environment vars

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
 
-export const supabase = createClient("https://alvmuuodakpsjwfvmspk.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsdm11dW9kYWtwc2p3ZnZtc3BrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc4NjE0OTIsImV4cCI6MjA1MzQzNzQ5Mn0.byppoIwq3NQMb9QkPY0pI1y0Cad8_jUEXHl4llPan4A")
+export const supabase = createClient(
+  'https://alvmuuodakpsjwfvmspk.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsdm11dW9kYWtwc2p3ZnZtc3BrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc4NjE0OTIsImV4cCI6MjA1MzQzNzQ5Mn0.byppoIwq3NQMb9QkPY0pI1y0Cad8_jUEXHl4llPan4A'
+);
