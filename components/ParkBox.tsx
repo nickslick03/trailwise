202a40c1b7487288fd2af313f8e96c7eb1df7fcf
hazellim1111 <artemishazel@gmail.com>
2025-04-30 09:20:13 -0400

    ParkBox component now redirects to park_description

--- components/ParkBox.tsx
+++ components/ParkBox.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 interface ParkBoxProps {
   id: string;
   name: string;
@@ -6,30 +8,25 @@ interface ParkBoxProps {
 
 const ParkBox: React.FC<ParkBoxProps> = ({ id, name, rules }) => {
   return (
-    <div
-      id={id}
-      className="bg-black text-white p-4 rounded-3xl shadow-lg w-[250px] sm:w-[300px] lg:w-[350px] min-h-[100px] max-h-[250px]
-        overflow-y-auto border border-transparent hover:border-2 hover:border-gray-500 hover:bg-gray-700 hover:shadow-xl
-        hover:scale-105 transition-all ease-in-out duration-300 mx-auto"
-    >
-      {/* Park Name */}
-      <h2 className="text-sm font-semibold mb-1 text-white">{name}</h2>
-
-      {/* Divider Line */}
-      <div className="border-t border-gray-600 my-1"></div>
-
-      {/* "Allows for" Label */}
-      <p className="mb-0.5 text-gray-300 text-xs">
-        <strong>Allows for:</strong>
-      </p>
-
-      {/* Rules List */}
-      <ul className="list-disc pl-4 text-gray-300 text-xs space-y-0.5">
-        {rules.slice(0, 3).map((rule, index) => (
-          <li key={index}>{rule}</li>
-        ))}
-      </ul>
-    </div>
+    <Link href="/park_description">
+      <div
+        id={id}
+        className="cursor-pointer bg-black text-white p-4 rounded-3xl shadow-lg w-[250px] sm:w-[300px] lg:w-[350px] min-h-[100px] max-h-[250px]
+          overflow-y-auto border border-transparent hover:border-2 hover:border-gray-500 hover:bg-gray-700 hover:shadow-xl
+          hover:scale-105 transition-all ease-in-out duration-300 mx-auto"
+      >
+        <h2 className="text-sm font-semibold mb-1 text-white">{name}</h2>
+        <div className="border-t border-gray-600 my-1"></div>
+        <p className="mb-0.5 text-gray-300 text-xs">
+          <strong>Allows for:</strong>
+        </p>
+        <ul className="list-disc pl-4 text-gray-300 text-xs space-y-0.5">
+          {rules.slice(0, 3).map((rule, index) => (
+            <li key={index}>{rule}</li>
+          ))}
+        </ul>
+      </div>
+    </Link>
   );
 };
 
